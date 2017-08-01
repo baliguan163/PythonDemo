7b0c7e380cd9f7e1d5cf310a47c9b54dbc7ea678
duliqun <duliqun>
2017-08-01 18:02:04 +0800

    new file

--- net/emial1.py
+++ net/emial1.py
@@ -1,27 +1,31 @@
 #coding=utf-8
 import smtplib
-from email.mime.multipart  import MIMEMultipart
-from email.mime.text  import  MIMEText
+import email.MIMEMultipart
+import email.MIMEText
 
+mail_host='stmp.163.com'
+mail_port='25'
+sender=r'baliguan163@163.com'
+send_pass='Baliguan#88'
+receiver=r'627277482@qq.com'
 
 from email.mime.base  import MIMEBase
 import os.path
-
-
 def mail():
     subject = '标题'
     ##创建Multiparty实例
-    msg = MIMEMultipart()
+    msg = email.MIMEMultipart()
     msg['Subject'] = subject
     msg['From'] = r'627277482@qq.com'
     msg['To'] = r'2901400977@qq.com'
     ##创建文本
-    content = MIMEText('content')
+    content = email.MIMEText('content')
     msg.attach(content)
     ##创建附件
-    xlsxpart = MIMEApplication(open('abc.xls', 'rb').read())
-    xlsxpart.add_header('Content-Dispositon','attachment',filename=‘abc.xls'))
+    xlsxpart = email.MIMEApplication(open('abc.xls', 'rb').read())
+    xlsxpart.add_header('Content-Dispositon','attachment',filename='abc.xls')
     msg.attach(xlsxpart)
+
     ##发送邮件
     contact = smtplib.SMTP_SSL(mail_host, mail_port)
     contact.login(sender, send_pass)
