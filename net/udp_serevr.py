773d25bdb57326cbfe7888c0ffecef1b1d64dbd4
Administrator <baliguan163@163.com>
2017-11-24 15:43:05 +0800

    更新消息

--- net/udp_serevr.py
+++ net/udp_serevr.py
@@ -6,5 +6,5 @@ while True:
     data,addr=udpserver.recvfrom(1024)
     print("来自",addr,"消息",data)
     sendata= (str(time.time()) + ":" + data.decode("utf-8")).encode("utf-8")
-    print("to",addr, "发送", sendata)
+    print("to",addr, "发送:", sendata)
     udpserver.sendto(sendata,addr)
