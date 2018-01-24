81df53040eec4968e4b1abaf4086a3f46c109c09
Administrator <baliguan163@163.com>
2018-01-24 16:14:17 +0800

    增加作者

---
+++ file/auto_cddh_pic.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+#######################################################
+#filename:
+#author:
+#date:xxxx-xx-xx
+#function：
+#######################################################
+import wda
+import io
+import urllib.parse
+import webbrowser
+import requests
+import time
+import base64
+
+c = wda.Client();
