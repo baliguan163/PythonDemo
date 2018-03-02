1981e8bb6569287db7e13acec8d2aca8bf64bdbd
Administrator <baliguan163@163.com>
2018-03-02 15:40:58 +0800

    增加作者

--- bs4/mm_jpg.py
+++ bs4/mm_jpg.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import threading
+
 _author__ = 'Administrator'
 
 import os
@@ -6,32 +8,51 @@ import urllib
 import requests
 from bs4 import BeautifulSoup
 
-# https://www.cnblogs.com/kmust/p/7113150.html
-def Schedule(a, b, c):
-    '''''
-    a:已经下载的数据块
-    b:数据块的大小
-    c:远程文件的大小
-   '''
-    per = 100.0 * a * b / c
-    if per > 100:
-        per = 100
-    print('%.2f%%' % per)
+session = requests.Session()
+#设置最大线程锁
+thread_lock = threading.BoundedSemaphore(value=10)
 
-def auto_down(url, filename):
-    file_dir = os.path.split(filename)[0]
-    print('file_dir:', file_dir)
-    if os.path.isdir(file_dir):
-        pass
+def download_list_pics(n,piclist,root):
+    for i in range(0, len(piclist)):
+        # print(len(piclist),'-',i+1,'  图集title:',piclist[i]['title'])
+        # print('图集src:', piclist[i]['src'])
+        # print('图集alt:', piclist[i]['alt'])
+        download_pics(n,i+1,piclist[i]['src'], root, piclist[i]['alt'])
+    # 解锁
+    thread_lock.release()
+
+
+#下载图片，并写入文件
+def download_pics(n,i,url,root,name):
+    # 请求头
+    headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+               'Accept-Encoding': 'gzip, deflate, sdch',
+               'Accept-Language': 'zh-CN,zh;q=0.8',
+               'Connection': 'keep-alive',
+               'Host': 'www.mmjpg.com',
+               'Referer':'http://img.mmjpg.com/2016/767/30.jpg',
+               'Upgrade-Insecure-Requests': '1',
+               'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
+
+    ir = session.get(url, headers=headers)
+    path = root + '\\'+str(name)+'.jpg'
+    # print('下载url:', url)
+    # print('下载status_code:', ir.status_code)
+    # # print('下载Location:',ir.headers["Location"])
+    # print('下载history:', ir.history)
+    if ir.status_code == 200:
+        print('下载ok:',n,' ',i,'' ,path)
+        with open(path, 'wb') as f:
+            f.write(ir.content)
     else:
-        os.makedirs(file_dir)
-    urllib.urlretrieve(url, filename, Schedule)
+        print('下载ng:', n, ' ', i, '', path)
+
 
 def get_html(url):
     try:
-        r = requests.get(url, timeout=30)
+        r = session.get(url, timeout=30)
         r.raise_for_status
-        # 该网站采用gbk编码！
+        #该网站采用gbk编码！
         r.encoding = 'utf8'
         return r.text
     except:
@@ -41,189 +62,126 @@ def get_html(url):
 def get_content(url):
     html = get_html(url)
     soup = BeautifulSoup(html, 'lxml')
-
     yi_list = soup.find('div', class_= 'pic')
-    # print('yi_list:', yi_list)
+
+
 
     # 找到列表
     title_list = yi_list.find_all('li')
+    total = yi_list.find_all('em', class_= 'info')
     # print('title_list:', title_list)
+    # total = yi_list.find_all('a', class_='last')
+    # print('total:', total)
+
+
+
     count=0
+    list=[]
     for mylist in  title_list:
         # print('mylist:', mylist)
         # print('mylist2:', mylist.find('a'))
         # print('mylist3:', mylist.find('span'))
         # <li><a href="http://www.mmjpg.com/mm/767" target="_blank"><img alt="绝色诱惑!身材超正的90后极品乳神于姬" height="330" src="http://img.mmjpg.com/small/2016/767.jpg" width="220"/></a><span class="title"><a href="http://www.mmjpg.com/mm/767" target="_blank">绝色诱惑!身材超正的90后极品乳神于姬</a></span><span>10-09 发布</span><span class="view">浏览(2917226)</span></li>
-
+        name = mylist.find('a').img['alt']
         picurl = mylist.find('a')['href']
+        count = count + 1
+        # print('----------------------------------------')
+        # print('图集名称:',count,' ',name )
+        # print('首图地址:', mylist.find('a').img['src'])
+        # print('图集地址:', picurl)
+        vid = {
+            'title': name,
+            'href': picurl,
+        }
+        list.append(vid)
+    return list;
 
-        count +1
-        print('------------------',count,'------------------')
-        print('图集名称:', mylist.find('a').img['alt'])
-        print('首图地址:', mylist.find('a').img['src'])
-        print('图集地址:', picurl)
+def get_page_content(url):
+    pichtml = get_html(url)
+    soup = BeautifulSoup(pichtml, 'lxml')
+    piclist = soup.find('div', class_='page')
+    # print('piclist:', piclist)
+    # 统计一共多少张图片
+    # myimg: 上一篇
+    # myimg: 2
+    # myimg: 3
+    # myimg: 4
+    # myimg: 5
+    # myimg: 6
+    # myimg: 38
+    # myimg: 下一张
+    piclist = piclist.find_all('a')
+    pic_len = len(piclist)
+    pic_index = piclist[pic_len - 2].text
+    # print('图集张数:', pic_index,'  图集地址:',url)
+    # for myimg in piclist.find_all('a'):
+    #     print('myimg:', myimg.text)
+    listpic = []
+    for i in range(1, int(pic_index) + 1):
+        mypicurl = url + '/' + str(i)
+        # print('mypicurl:', mypicurl)
+        # print('savepath:', savepath)
+        listpic.append(mypicurl)
 
-        pichtml = get_html(picurl)
+    list = []
+    for i in range(0, len(listpic)):
+        # print('mypicurl:', listpic[i])
+        pichtml = get_html(listpic[i])
         soup = BeautifulSoup(pichtml, 'lxml')
-        piclist = soup.find('div', class_='page')
-        # print('piclist:', piclist)
 
-        # 统计一共多少张图片
-        # myimg: 上一篇
-        # myimg: 2
-        # myimg: 3
-        # myimg: 4
-        # myimg: 5
-        # myimg: 6
-        # myimg: 38
-        # myimg: 下一张
-        piclist = piclist.find_all('a')
-        pic_len = len(piclist)
-        pic_index = piclist[pic_len-2].text
-        print('图集张数:',pic_index)
-        # for myimg in piclist.find_all('a'):
-        #     print('myimg:', myimg.text)
-        for i in range(1,int(pic_index)+1):
-            mypicurl = picurl + '/' + str(i)
-            savepath = 'D:\\' + str(i)
-            print('mypicurl:', mypicurl)
-            print('savepath:', savepath)
-            auto_down(mypicurl,savepath)
+        title = soup.find('div', class_='article').find('h2').text
+        downpath = soup.find('div', class_='content').find('a').find('img')['src']
+        alt = soup.find('div', class_='content').find('a').find('img')['alt']
+        # print('title:', title)
+        # print('src:', downpath, ' alt:', alt)
+
+        vid = {
+            'title': title,
+            'src': downpath,
+            'alt': alt,
+        }
+        list.append(vid)
+    return list
 
 
+def create_dir(directory):
+	isExists = os.path.exists(directory)
+	if not isExists:
+		os.makedirs(directory)
+	return directory
 
+def get_all_tag(url):
+    pichtml = get_html(url)
+    soup = BeautifulSoup(pichtml, 'lxml')
+    piclist = soup.find('div', class_='subnav').find_all('a')
+    list=[]
+    for i in range(1,len(piclist)):
+        print('href:', piclist[i]['href'],' ',piclist[i].text)
+        vid = {
+            'title': piclist[i].text,
+            'href': piclist[i]['href'],
+        }
+        list.append(vid)
+    return list
 
-        # print('mylist3:', mylist.find('span').text)
-        # print('mylist3:', mylist.find('span').a['href'])
-
-
-    # print('title_list:', len(title_list), ' ', title_list)
-    # print('time_list:', len(time_list), ' ', time_list)
-    # print('time_list:', time_list)
-    # print('tag_list :', tag_list)
-    # print('url_list :', url_list,' len:',len(url_list))
-
-    # for i in range(0, len(title_list)):
-    #
-    #     bu_men = title_list[i].find('span',class_ ='red').contents
-    #     time = title_list[i].find('span', class_ ='goRight').contents
-    #     href = title_list[i].find('a')['href']
-    #     content = title_list[i].find('a').contents
-    #     newurl = 'http://www.yangxian.gov.cn' + href
-    #
-    #     # print('href:', href)
-    #     print('---------------------', i + 1, '---------------------')
-    #     print('新闻标题:',  len(content), content[0])
-    #     print('新闻地址:', newurl)
-    #     print('发布者:', time[0], bu_men[0])
-    #
-    #     newHtml = get_html(newurl)
-    #     soupHtml = BeautifulSoup(newHtml, 'lxml')
-    #     news_list = soupHtml.find('div', class_='contentLeft')
-    #     # print('news_list:', news_list)
-    #
-    #     # print('新闻标题:', soupHtml.find('h1').text)
-    #
-    #     for myimg in news_list.find_all('img'):
-    #         img_src = myimg.get('src')
-    #         print('图片地址:', img_src)
-    #
-    #     spanTemp = ''
-    #     for myspan in news_list.find_all('span'):
-    #         # print('myspan:', myspan.get_text().strip())
-    #         spanTemp = spanTemp + myspan.get_text().strip() + ' '
-    #
-    #     print('新闻标记:', spanTemp)
-    #     info = soupHtml.find('div', class_='info').get_text().strip().replace('\n', '')
-    #     print('新闻内容:\n',info)
-
-
-
-        # for i in range(0, len(imgs)):
-        #     print('imgs:', imgs[i])
-
-        # whos = title_list[i].find('font').contents
-        # print('whos:', len(whos), whos[0])
-        #
-        # url = title_list[i].find('a')['href']
-        # print('url:', url)
-        #
-        # newurl = 'http://www.yangxian.gov.cn' + url
-        # print('newurl:', newurl)
-        #
-        # times = time_list[i].find('span').contents
-        # print('time:', len(times), times[0])
-
-        # urls = url_list.find('span').contents
-        # print('urls:', urls[0])
-
-
-        # for title in titles:
-        #     print('title:', title)
-        #     time = title.find('href').text
-        #     print('href:', time[0].contents )
-
-
-        # urls = url_list.find_all('span')
-        # count = 0
-        # for top in urls:
-        #     print('top:',top)
-        #     # # 找到图片连接，
-        #     # img_url = top.find('href')
-        #     # print('img_url :', img_url)
-        #     #
-        #     name = top.find('span')
-        #     print('name:', name)
-
-        # # 这里做一个异常捕获，防止没有上映时间的出现
-        # try:
-        #     time = top.find('span', class_='sIntro').text
-        # except:
-        #     time = "暂无上映时间"
-        #
-        # # 这里用bs4库迭代找出“pACtor”的所有子孙节点，即每一位演员解决了名字分割的问题
-        # try:
-        #     actors = top.find('p', class_='pActor')
-        #     # print('actors:',actors)
-        #     actor = ''
-        #     for act in actors.contents:
-        #         actor = actor + act.string + ' '
-        # except:
-        #     actor = "暂无导演"
-        #
-        # # 找到影片简介
-        # intro = top.find('p', class_='pTxt pIntroShow').text.replace('\n', '')
-        # # intro = top.find('p', class_='pTxt pIntroHide').text
-        #
-        # count += 1
-        # print('--------------------------------------------------------------')
-        # print(count)
-        # print('影片地址:', href)
-        # print('图片连接:', img_url)
-        # print("片名:{}\n{}\n{}\n{}\n ".format(name, time, actor, intro))
-
-
-
-        # print(count, 'url:'.join(img_url))
-
-        # 我们来吧图片下载下来：
-        # with open('dianying_img/'+name+'.png','wb+') as f:
-        #   f.write(requests.get(img_url).content)
-
-
-# http://www.yangxian.gov.cn/search/searchResult.jsp?t_id=178&site_id=CMSyx&q=八里关&btn_search=搜索&p=1
-# http://www.yangxian.gov.cn/search/searchResult.jsp?t_id=178&site_id=CMSyx&q=八里关&btn_search=搜索&p=2
-# #抓取一页数据
-# def fetch(id=1,debug=True):
-#     key='八里关'
-#     query_data = {'t_id': 178,'site_id': 'CMSyx','q': key,'btn_search': '搜索','p': id}
-#     url = 'http://www.yangxian.gov.cn/search/searchResult.jsp' + '?' + get_query_string(query_data)
-#     #urlbase = r'http://www.yangxian.gov.cn/search/searchResult.jsp?t_id=178&site_id=CMSyx&q=八里关&btn_search=搜索&p='
-#     #url = urlbase + str(id)
-#     print('获取地址:',url)
 def main():
-    url = 'http://www.mmjpg.com/tag/tgod'   #推女神
-    get_content(url)
+    root_dir = create_dir('D:\\www.mmjpg.com\\')  # 绝对路径
+    url = 'http://www.mmjpg.com/top'
+    taglist = get_all_tag(url)
+    for i in range(0,len(taglist)):
+        # print('title:', taglist[i]['title'])
+        root_dir_2 = create_dir(root_dir +  taglist[i]['title']+ '\\') # 绝对路径
+        list = get_content(url)
+        print('图集个数:',len(list))
+        for i in range(0,len(list)):
+            print('下载图集:',i+1,' ', list[i]['title'],list[i]['href'])
+            piclist = get_page_content(list[i]['href'])
+            print('下载数量:', len(piclist))
+            root_dir_3 = create_dir(root_dir_2 + list[i]['title'])  # 绝对路径
+            #上锁
+            thread_lock.acquire()
+            t = threading.Thread(target=download_list_pics, args=(i+1,piclist,root_dir_3))
+            t.start()
 
 if __name__ == "__main__":
     main()
