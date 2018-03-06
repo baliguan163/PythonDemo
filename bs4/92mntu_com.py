5d369173a04079b747771685b688f5c0a47d3211
Administrator <baliguan163@163.com>
2018-03-07 01:24:00 +0800

    添加文件

--- bs4/92mntu_com.py
+++ bs4/92mntu_com.py
@@ -42,53 +42,52 @@ def get_page_pic_all_url(url):
     return list
 
 
-# 一个图集有多g
+# 一个图集有多
 def get_page_pic_href(sum,page,url,dir):
-
     html = get_html(url)
     soup = BeautifulSoup(html, 'lxml')
-    pagelist = soup.find('div', class_='pages')
+    pagelist = soup.find('div', class_='pageart')
     pagelist = pagelist.find_all('li')
+
     # print('li:', pagelist)
     # for i in range(0, len(pagelist)):
     #     print('src:',pagelist[i].find('a').text)
 
     pagecount0 = pagelist[0].find('a').text
-    # pagecount3= pagelist[3].find('a')['href']
     pagecount0 = pagecount0[1:len(pagecount0)-3]
-    # pagecount3 = pagecount3[0:len(pagecount3) - 7]
+
     pagecounturl = url[0:len(url)-5]
-    # print('页数:', pagecount0,' ', pagecounturl)
+    print('页数:', pagecount0,' ', pagecounturl)
 
     #一个图集有多少页地址
     list = []
-    list.append(url)
+    list.append(url+'.html')
     for i in range(2, int(pagecount0) + 1):
         href = pagecounturl + '_'+ str(i) + '.html'
-        # print('href:', i, '', href)
+        print('href:', i, '', href)
         list.append(href)
 
     print('  下载图集:',sum,'-', page, ' ',len(list),'页  ', url, ' 目录:', dir)
 
 
-    # 一个图集所有页图片地址
-    allpiclist = []
-    for i in range(0, len(list)):
-        piclist = get_page_pic_all_url(list[i])
-        allpiclist = allpiclist + piclist
-    # print('  图集图片数量:', len(allpiclist))
-
-    # 开始下载
-    for i in range(0, len(allpiclist)):
-        # print('  下载图片:',sum,'-', page,' ',len(allpiclist),'-',i+1, '', allpiclist[i])
-        download_pics(sum,page,len(allpiclist),i+1,allpiclist[i],dir,i+1)
+    # # 一个图集所有页图片地址
+    # allpiclist = []
+    # for i in range(0, len(list)):
+    #     piclist = get_page_pic_all_url(list[i])
+    #     allpiclist = allpiclist + piclist
+    # # print('  图集图片数量:', len(allpiclist))
+    #
+    # # 开始下载
+    # for i in range(0, len(allpiclist)):
+    #     # print('  下载图片:',sum,'-', page,' ',len(allpiclist),'-',i+1, '', allpiclist[i])
+    #     download_pics(sum,page,len(allpiclist),i+1,allpiclist[i],dir,i+1)
 
         # print(len(pagelist), '-', i + 1, '  src:', pagelist[i].find('a')['href'])
         # print('图集src:', piclist[i]['src'])
         # print('图集alt:', piclist[i]['alt'])
     #     download_pics(n,i+1,piclist[i]['src'], root, piclist[i]['alt'])
     # 解锁
-    thread_lock.release()
+    # thread_lock.release()
 
 
 #下载图片，并写入文件
@@ -222,8 +221,9 @@ def main():
             tags_list =  tags_list + page_list
         print('图集总数:', len(tags_list))
         for i in range(0, len(tags_list)):
-            root_dir_3 = create_dir(root_dir + tags_list[i]['text'] + '\\')  # 绝对路径
+            root_dir_3 = create_dir(root_dir_2 + tags_list[i]['text'] + '\\')  # 绝对路径
             print('  开始下载图集:', len(tags_list),'-',i+1, ' ', tags_list[i]['text'], tags_list[i]['href'])
+            get_page_pic_href(len(tags_list), i+1, tags_list[i]['href'], root_dir_3)
 
     # for i in range(0, len(tujilist)):
     #     # print('开始下载图集:', i+1, ' ', tujilist[i]['title'], tujilist[i]['href'])
