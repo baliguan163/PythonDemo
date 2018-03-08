fed8f7a116dbd271d33232c8d0e2288c0d3045af
Administrator <baliguan163@163.com>
2018-03-08 22:50:32 +0800

    修改文件

--- bs4/www_qqretu_com.py
+++ bs4/www_qqretu_com.py
@@ -83,7 +83,7 @@ class spider(object):
                    }
 
         try:
-            # r = session.get(url, headers=headers)
+            # r = requests.get(url, headers=headers)
             # r = requests.get(url,timeout=3)
             r = requests.get(url)
             # r.raise_for_status
@@ -119,7 +119,7 @@ class spider(object):
         html = picspider.get_html(url)
         soup = BeautifulSoup(html, 'lxml')
 
-        tuji_list = []
+        pages_list = []
         try:
             pagelist = soup.find('div', class_='Pages')
             pagelist = pagelist.find_all('ul')
@@ -130,91 +130,84 @@ class spider(object):
                 # for k in range(0,len(textlist)):
                 #     pagecount0 = textlist[k]
                 #     print('  src:', pagecount0['href'])
-
                 pagecount0 = textlist[len(textlist) - 1]['href']
                 # print('  图集页数pagecount0:',pagecount0)
                 sum = pagecount0[0:len(pagecount0) - 5]
-                print('  图集总页数:', url,' ', sum )
-
+                # print('  图集页数:', sum, '', url)
                 # 一个图集有多少页地址
-                pages_list = []
+
                 for i in range(1, int(sum) + 1):
                     href = url + str(i) + '.html'
                     # print('href:', i, '', href)
                     pages_list.append(href)
-
-                sumcount = 0
-                for j in range(0, len(pages_list)):
-                    html = picspider.get_html(pages_list[j])
-                    soup = BeautifulSoup(html, 'lxml')
-                    try:
-                        pagelist = soup.find('div', class_='ImagesList ImagesList6')
-                        pagelist = pagelist.find_all('li')
-                        # print('  pagelist:', pagelist)
-                        p = 0
-                        for k in range(0, len(pagelist)):
-                            href = pagelist[k].find('a')['href']
-                            title = pagelist[k].find('a')['title']
-                            # print('  图集:',sum,'',title,' ',href)
-                            vid3 = {'title': title, 'href': href}
-                            tuji_list.append(vid3)
-
-                            print('  sum:', sum, '-', j + 1, pages_list[j], ' ', p, ' ', sumcount)
-                            picspider.get_page_pic_urls(sum,k+1,href)
-
-                            sumcount = sumcount + 1
-                            p = p + 1
-
-                        # print('  sum:',sum,'-',j+1, pages_list[j],' ', p,' ',sumcount)
-                    except:
-                        print('  获取图集总数异常：', pages_list[j])
-            return tuji_list
+            # print('  图集页数:', len(pages_list), '', url )
         except:
-            print('  获取图集页码数异常：',url)
+            print('  图集页数异常：',url)
+        return pages_list
+
+
+        # 获取二级分类所有页的url信息
+    def get_tuji_all_tu_info(self, url):
+        # print('寄:', url)
+        html = picspider.get_html(url)
+        soup = BeautifulSoup(html, 'lxml')
+
+        tuji_list=[]
+        try:
+            pagelist = soup.find('div', class_='ImagesList ImagesList6')
+            pagelist = pagelist.find_all('li')
+            # print('  pagelist:', pagelist)
+            for k in range(0, len(pagelist)):
+                href = pagelist[k].find('a')['href']
+                title = pagelist[k].find('a')['title']
+                # print('  下载图集:',k+1,' ',title,' ',href)
+                vid3 = {'title': title, 'href': href}
+                tuji_list.append(vid3)
+        except:
+            print('  图集信息异常：', url)
         return tuji_list
 
-        # 下载图片，并写入文件
-    def download_pics(self,sum, page, pagesum, i, url, root, name):
+
+    # 下载图片，并写入文件
+    def download_pics(self,sum, i, url, root, name):
         offset = url
-            # 请求头
+        # 请求头
         headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                       'Accept-Encoding': 'gzip, deflate',
-                       'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
-                       'Connection': 'keep-alive',
-                       'Host': '92mntu.com',
-                       'Upgrade-Insecure-Requests': '1',
-                       'Referer': offset,
-                       'Cookie': 'a9449_times=2; aa=123; a9449_pages=6; __tins__19179449={"sid": 1520414864125, "vd": 5, "expires": 1520417480260}; __51cke__=; __51laig__=6',
-                       'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0'
-                       }
-        path = root + '\\' + str(name) + '.jpg'
+                   'Accept-Encoding': 'gzip, deflate',
+                   'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+                   'Connection': 'keep-alive',
+                   'Host': 'www.qqretu.com',
+                   'Upgrade-Insecure-Requests': '1',
+                   'Referer': url,
+                   'Cookie': ' yunsuo_session_verify=2d7a6d9a1ad4698d516489b3d9353c3a; Hm_lvt_faefe41d3874cd24881ac392f4df634d=1520489947; Hm_lpvt_faefe41d3874cd24881ac392f4df634d=1520495727',
+                   'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0'
+                   }
+
+        path = root + '\\' + name + '.jpg'
         # print('下载:', url)
         isExists = os.path.exists(path)
         if not isExists:
-            url = url[0:len(url) - 1]
-            print('下载:', url)
+            # url = url[0:len(url) - 1]
+            # print('下载:', url)
 
             # print('下载:', url)
-            ir = session.get(url)
+            ir = session.get(url,timeout=3)
             if ir.status_code == 200:
-                print('  下载ok:', sum, '-', page, ' ', pagesum, '-', i, '', url, ' ', path)
+                print('  下载ok:', sum, '-', i, '', url, ' ', path)
                 with open(path, 'wb') as f:
                     f.write(ir.content)
                     f.close()
             else:
-                print('  下载ng:', ir.status_code, ' ', sum, '-', page, ' ', pagesum, '-', i, '', url, ' ', path)
+                print('  下载ng:', ir.status_code, ' ', sum, '-', i, '', url, ' ', path)
         else:
-            print('  存在不下载:', sum, '-', page, ' ', pagesum, '-', i, '', url, ' ', path)
+            print('  存在不下载:', sum, '-', i, '', url, ' ', path)
 
 
-
-
-
-        # 获取一页的图片地址
-    def get_page_pic_urls(self,sum,p,url):
+    def get_tuji_page_sum(self,url):
         # print('get_tuji_urls:', url)
         html = picspider.get_html(url)
         soup = BeautifulSoup(html, 'lxml')
+        list = []
 
         try:
             pagelist = soup.find('div', class_='Pages')
@@ -236,47 +229,44 @@ class spider(object):
                 # # print('  图集页数pagecount0:',pagecount0)
             sum = pagecount0[1:len(pagecount0) - 3]
             baseurl = url[0:len(url) - 5]
-            print('  图集总页数:', url,' ', sum ,' ',url,' ',baseurl)
 
-            list = []
             list.append(url)
             for i in range(2, int(sum) + 1):
                 href = baseurl + '_' + str(i) + '.html'
-                # print('  图片地址:',sum,'-', i, '', href)
+                # print('    图集地址:',sum,'-', i, '', href)
                 list.append(href)
 
-            pic_list = []
-            try:
-                for k in range(0,len(list)):
-                    html = picspider.get_html(list[k])
-                    soup = BeautifulSoup(html, 'lxml')
-
-                    titlelist = soup.find('div', class_='articleV3Title')
-                    title = titlelist.find('h1').text
-                    # print('title:', title)
-
-                    pagelist = soup.find('div', class_='articleV3Body id6')
-                    pagelist = pagelist.find_all('a')
-                    # print('pagelist:', pagelist)
-                    for i in range(0, len(pagelist)):
-                        # print('  alt:',pagelist[i].find('img')['alt'])
-                        # print('  src:', pagelist[i].find('img')['src'])
-                        # alt = pagelist[i].find('img')['alt']
-                        src = pagelist[i].find('img')['src']
-                        vid3 = {'title': title, 'src': src}
-                        pic_list.append(vid3)
-                        print('  图集:', sum, '-', p, ' 图片', len(list), '-', k + 1, ' ', i + 1, ' ', list[k], ' ', title,' ', src)
-                        picspider.download_pics(sum,p,len(list),list[k],'D:\www.duotoo.com',title)
-
-                return tuji_list
-            except:
-                print('获取页码异常：', list[k])
-
+            # print('    图集总页数:', sum , ' ',url, ' ', baseurl)
         except:
             print('获取页码数异常：', url)
+        return list
 
 
-        return tuji_list
+    def get_tuji_page_all_pic_info(self,url):
+        # print('get_tuji_page_all_pic_info:', url)
+        html = picspider.get_html(url)
+        soup = BeautifulSoup(html, 'lxml')
+        pic_list = []
+        try:
+            titlelist = soup.find('div', class_='articleV3Title')
+            title = titlelist.find('h1').text
+            # print('title:', title)
+            pagelist = soup.find('div', class_='articleV3Body id6')
+            pagelist = pagelist.find_all('a')
+            # print('pagelist:', pagelist)
+            for p in range(0, len(pagelist)):
+                    # print('  alt:',pagelist[i].find('img')['alt'])
+                    # print('  src:', pagelist[i].find('img')['src'])
+                    # alt = pagelist[i].find('img')['alt']
+                src = pagelist[i].find('img')['src']
+                print('     图片地址:',  p + 1, ' ', title, '  ', src)
+                vid3 = {'title': title, 'src': src}
+                pic_list.append(vid3)
+                # print('  图集:', sum, '-', p, ' 图片', len(list), '-', k + 1, ' ', i + 1, ' ', list[k], ' ', title,' ', src)
+                # picspider.download_pics(sum, p, len(list), list[k], 'D:\www.duotoo.com', title)
+        except:
+            print('  获取页码数异常：', url)
+        return pic_list
 
 
 
@@ -350,64 +340,39 @@ if __name__ == '__main__':
         text = list_1[i]['text']
         href = list_1[i]['href']
         root_dir_1 = picspider.create_dir(root_dir + text + '\\')
-        print('分类:', text, ' ', href,'   save:',root_dir_1)
+        # print('图集分类:', text, ' ', href,'   save:',root_dir_1)
 
-        #获取所有图集信息
+        #获取所有图集信息: 图集页多少
         tuji_list = picspider.get_tuji_urls(href)
         # print('分类:', text, ' ', href, ' 图集数:', len(tuji_list))
         time.sleep(1)
 
-        # # 获取所有图集的所有照片信息
-        # for j in range(0, len(tuji_list)):
-        #     pic_list = picspider.get_page_pic_urls(tuji_list[j]['href'])
-        #     time.sleep(1)
+        # 获取所有图集的所有图集信息: 多少个图集
+        tuji_all_info = []
+        for j in range(212, len(tuji_list)):
+            pic_list = picspider.get_tuji_all_tu_info(tuji_list[j])
+            tuji_all_info = tuji_all_info + pic_list
+            # print('  图集分类:', text, ' ', tuji_list[j], ' 图集页数:', len(tuji_list),'-',j+1,' 已经下载图集总数', len(tuji_all_info))
+            # time.sleep(1)
+        print('  图集分类:', text, ' ', href, ' 图集页数:', len(tuji_list),' 已经下载图集总数', len(tuji_all_info))
 
-    #     # 一类分类
-    #     for j in range(0, len(tag_1_name)):
-    #         tag_name = tag_1_name[j]['text']
-    #         tag_href = tag_1_name[j]['href']
-    #         print('1:',  tag_name,' ',tag_href)
-    #
-    #     # 二级分类
-    #     for k in range(0, len(tag_1_href)):
-    #         tag_name = tag_1_href[k]['text']
-    #         tag_href = tag_1_href[k]['href']
-    #
-    #         #二级分类页码数
-    #         page_url_list = picspider.get_2_tuji_urls(tag_href)
-    #         print('  图集:', tag_name,'  页数', len(page_url_list),' ',tag_href)
-    #
-    #         #二级分类页码中所有图集信息
-    #         tuji_url_list = []
-    #         for m in range(0,len(page_url_list)):
-    #             tujilist = picspider.get_tuji_urls(page_url_list[m])
-    #             tuji_url_list = tuji_url_list + tujilist
-    #             time.sleep(1)
-    #         print('图集:', tag_name, ' 页数', len(page_url_list), '  图集数:', len(tuji_url_list), '', tag_href)
-    #
-    #
-    #         #每一个图集页码数列表
-    #         for n in range(0, len(tuji_url_list)):
-    #             title = tuji_url_list[n]['title']
-    #             href  = tuji_url_list[n]['href']
-    #             root_dir_2 = picspider.create_dir(root_dir_1 + title + '\\')
-    #             print('图集title:',title,' ',href)
-    #             # picspider.get_tuji_pages(href)
-    #
-    #         # 每一个图集的下载图片列表
-    #         # 下载每一个图集下的图片
-    #
-    #             # for m in range(0, len(tuji_url_list)):
-    #             #
-    #             #     time.sleep(2)  # 休眠1秒
-    #
-    #         time.sleep(1)
+        #获取一个图集有多少页: 页多少
+        for m in range(0,len(tuji_all_info)):
+            root_dir_2 = picspider.create_dir(root_dir_1 + tuji_all_info[m]['title'] + text + '\\')
+            page_sum_list = picspider.get_tuji_page_sum(tuji_all_info[m]['href'])
+            print('  图集:',tuji_all_info[m]['title'],' ',tuji_all_info[m]['href'], '  图集页码总数', len(page_sum_list))
+            # time.sleep(1)
 
+            # 下载图集信息
+            pic_list = []
+            for n in range(0, len(page_sum_list)):
+                page_pic_info_list = picspider.get_tuji_page_all_pic_info(page_sum_list[n])
+                pic_list = pic_list + page_pic_info_list
+            print('  下载图图片数量', len(pic_list))
 
-
-    # all_links = picspider.changepage(url, 3)
-    # for link in all_links:
-    #     picspider.ppic(link)
+            # 下载图片
+            for k in range(0, len(pic_list)):
+                picspider.download_pics( len(pic_list), n+1, pic_list[k]['src'], root_dir_2, pic_list[k]['title'])
 
     end = time.time()
     print('耗时:{}'.format(end  - start))
